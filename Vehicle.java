f4a6e4ad6822e319bc0a40ce5d12e3ab4cededcb
Warren Smith <wsmith2278@gmail.com>
2025-04-04 21:32:00 -0400

    refactored constructor in Vehicle class to improve code clarity (Task 1)

--- Vehicle.java
+++ Vehicle.java
@@ -8,20 +8,22 @@ public abstract class Vehicle {
     public enum VehicleStatus { AVAILABLE, RESERVED, RENTED, MAINTENANCE, OUTOFSERVICE }
 
     public Vehicle(String make, String model, int year) {
-    	if (make == null || make.isEmpty())
-    		this.make = null;
-    	else
-    		this.make = make.substring(0, 1).toUpperCase() + make.substring(1).toLowerCase();
-    	
-    	if (model == null || model.isEmpty())
-    		this.model = null;
-    	else
-    		this.model = model.substring(0, 1).toUpperCase() + model.substring(1).toLowerCase();
+    	// Simplified using helper method capitalize().
+		this.make =	capitalize(make);
+    	this.model = capitalize(model);
     	
         this.year = year;
         this.status = VehicleStatus.AVAILABLE;
         this.licensePlate = null;
     }
+
+    // Helper method capitalize() that capitalizes the first letter and makes the rest lowercase.
+	private String capitalize(String input) {
+		if (input == null || input.isEmpty()) {
+			return null;
+		}
+		return input.substring(0, 1).toUpperCase() + model.substring(1).toLowerCase();
+	}
 
     public Vehicle() {
         this(null, null, 0);
